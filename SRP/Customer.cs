6368a92adf40a92493446f9a7b42aa95db2faaf8
Sahul Hameed <sahulhameed9094@gmail.com>
2025-05-07 16:49:01 +0530

    Add project files.

---
+++ SRP/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SRP
+{
+    public class Customer
+    {
+        public int id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+
+
+        public void Add(Customer customer)
+        {
+
+        }
+
+        public List<Customer> GetAll()
+        {
+            return new List<Customer>();
+        }
+    }
+}
